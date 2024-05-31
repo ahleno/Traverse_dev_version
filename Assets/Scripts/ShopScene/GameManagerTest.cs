a9355068a6cc741fe495621c0e5cd5eac8de97bf
ckdmswo17 <ckdmswo17@naver.com>
2024-05-31 15:24:47 +0900

    add gamemanger

---
+++ Assets/Scripts/ShopScene/GameManagerTest.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManagerTest : MonoBehaviour
+{
+    public int yeopjeon;
+    public GameObject wearingCap;
+    public GameObject wearingCloth;
+    private string inventory;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
