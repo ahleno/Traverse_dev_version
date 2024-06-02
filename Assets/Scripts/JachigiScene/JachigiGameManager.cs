05a7a7902718831bd943f629c754899a865f345f
상호 이 <2000rhehcl@naver.com>
2024-06-02 21:49:45 +0900

    GameManager Update

--- Assets/Scripts/JachigiScene/JachigiGameManager.cs
+++ Assets/Scripts/JachigiScene/JachigiGameManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1cb1c832e7ce8f784db76a9a69dd9e579d902bc00b13f4a91cb2f589eace3804
-size 2698
+oid sha256:bd68ec48bbfd6766ecc731691c38301cc71d30229f8027e6c1a45e594621c351
+size 2911
