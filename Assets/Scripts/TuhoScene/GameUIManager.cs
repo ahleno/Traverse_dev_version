eeb7ca708ded77f4649fc1f479338d123f5abdd0
Han-eol <104002983+ahleno@users.noreply.github.com>
2024-08-14 20:18:21 +0900

    Tuho gameMode setting

--- Assets/Scripts/TuhoScene/GameUIManager.cs
+++ Assets/Scripts/TuhoScene/GameUIManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4f0dc248df71244a536f1b5bfd22196925288acd9143c8f45adcf96aba077450
-size 1960
+oid sha256:33405ace84b6081ffda13db6485fc5575700692d6916329bdf779e0de65cb1a4
+size 1951
--- Assets/Scripts/TuhoScene/TuhoGameManager.cs
+++ Assets/Scripts/TuhoScene/TuhoGameManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c6a59c5df43710d5c7b1c3d295f69c791c2c8ccf2a1e9a3035c00bcfa4d7af05
-size 287
+oid sha256:7532c5ce777d261aecfc67c3ca996789334f4e1f06e6f2e0332efd587ee8a719
+size 14587
